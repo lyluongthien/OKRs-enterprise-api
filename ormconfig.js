88e2a60316200c4ee89a5aaae385b928f9135831
Harry Tran <nhattq.coding@gmail.com>
2020-06-23 21:51:57 +0700

    chore: update scripts

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,24 @@
-import accessEnv from './src/services/helpers/accessEnv';
+// const accessEnv = require('')
 
-const type = 'postgres';
-const host = accessEnv('DB_HOST', null);
-const username = accessEnv('DB_USER', null);
-const password = accessEnv('DB_PASSWORD', null);
-const database = accessEnv('DB_NAME', null);
-const isDev = accessEnv('NODE_ENV', null);
+// const type = 'postgres';
+// const host = accessEnv('DB_HOST', null);
+// const username = accessEnv('DB_USER', null);
+// const password = accessEnv('DB_PASSWORD', null);
+// const database = accessEnv('DB_NAME', null);
+// const isDev = accessEnv('NODE_ENV', null);
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  type,
-  host,
-  username,
-  password,
-  database,
-  entities: [__dirname + '../modules/**/*.entity.ts'],
-  migrations: [__dirname + '../modules/**/*.entity.ts'],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/src/db/entities/*.entity.{ts,js}'],
+  migrations: ['src/db/migrations/*.{ts,js}'],
   cli: {
-    migrationsDir: __dirname + './src/db/migrations',
+    entitiesDir: 'src/db/entities',
+    migrationsDir: 'src/db/migrations',
   },
   migrationsRun: true,
   synchronize: isDev ? true : false,
