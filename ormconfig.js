386f36eea4c4b24b343f725504b72e0f72766e31
Harry Tran <nhattq.coding@gmail.com>
2020-06-23 22:24:48 +0700

    chore: update scripts

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,14 @@
-// const accessEnv = require('')
-
-// const type = 'postgres';
-// const host = accessEnv('DB_HOST', null);
-// const username = accessEnv('DB_USER', null);
-// const password = accessEnv('DB_PASSWORD', null);
-// const database = accessEnv('DB_NAME', null);
-// const isDev = accessEnv('NODE_ENV', null);
 const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   type: 'postgres',
+  charset: 'utf8mb4',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: ['dist/src/db/entities/*.entity.{ts,js}'],
-  migrations: ['src/db/migrations/*.{ts,js}'],
+  migrations: ['dist/src/db/migrations/*.{ts,js}'],
   cli: {
     entitiesDir: 'src/db/entities',
     migrationsDir: 'src/db/migrations',
--- src/db/migrations/1592904360524-users.ts
+++ src/db/migrations/1592904360524-users.ts
@@ -1,11 +1,43 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class users1592904360524 implements MigrationInterface {
+  private userTable = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isPrimary: true,
+        isGenerated: true, // Auto-increment
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        length: '255',
+        isUnique: true,
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.userTable);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.userTable);
+  }
 }
