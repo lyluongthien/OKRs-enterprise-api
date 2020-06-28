48b21e79cd90a5c0e6071f384e2efe44a7283f4a
Trung Tran <yusaki@Trungs-MacBook-Pro.local>
2020-06-28 18:55:25 +0700

    feat: add user features

--- src/db/dbConnection.service.ts
+++ src/db/dbConnection.service.ts
@@ -1,12 +1,14 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import accessEnv from '@helpers/accessEnv';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConfig } from 'src/constants/Enums';
+import accessEnv from '@libs/accessEnv';
 
-const type = 'postgres';
-const host = accessEnv('DB_HOST', null);
-const username = accessEnv('DB_USER', null);
-const password = accessEnv('DB_PASSWORD', null);
-const database = accessEnv('DB_NAME', null);
+const type = DbConfig.DB_TYPE;
+const host = accessEnv(DbConfig.DB_HOST, '0.0.0.0');
+const port = +accessEnv(DbConfig.DB_PORT, null);
+const username = accessEnv(DbConfig.DB_USER, null);
+const password = accessEnv(DbConfig.DB_PASSWORD, null);
+const database = accessEnv(DbConfig.DB_NAME, null);
 
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
@@ -14,6 +16,7 @@ export const databaseProviders = [
       return {
         type,
         host,
+        port,
         username,
         password,
         database,
--- src/db/entities/user.entity.ts
+++ src/db/entities/user.entity.ts
@@ -1,5 +1,12 @@
-import { Entity, BaseEntity, PrimaryColumn, Column } from 'typeorm';
-import { TableName } from '../../constants/Enums';
+import bcrypt from 'bcryptjs';
+import { Entity, BaseEntity, PrimaryColumn, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
+import { TableName } from '@constants/Enums';
+import { JwtPayload, UserToken } from '@constants/Interfaces';
+import accessEnv from '@libs/accessEnv';
+import { createJWT } from '@libs/jwt';
+
+const SALT_WORK_FACTORY = accessEnv('SALT_WORK_FACTORY', 11);
+const _salt = bcrypt.genSaltSync(+SALT_WORK_FACTORY);
 
 @Entity({ name: TableName.User })
 export class UserEntity extends BaseEntity {
@@ -8,4 +15,57 @@ export class UserEntity extends BaseEntity {
 
   @Column()
   email: string;
+
+  password: string;
+
+  @Column()
+  passwordHashed: string;
+
+  @Column()
+  _salt: string;
+
+  @Column()
+  resetPasswordToken: string;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  avatarURL: string;
+
+  @Column()
+  gravatarURL: string;
+
+  @Column()
+  isActive: boolean;
+
+  @Column()
+  isApproved: boolean;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    this.passwordHashed = bcrypt.hashSync(this.password, _salt);
+    delete this.password;
+  }
+
+  @BeforeUpdate()
+  async updateHashPassword(): Promise<void> {
+    if (this.passwordHashed !== this.passwordHashed) {
+      this.passwordHashed = bcrypt.hashSync(this.passwordHashed, _salt);
+    }
+  }
+
+  async verifyPassword(inputPassword: string): Promise<boolean> {
+    return bcrypt.compare(inputPassword, this.passwordHashed);
+  }
+
+  generateToken(): UserToken {
+    const expiresIn: number = 30 * 24 * 60 * 60; // 1 month
+    const payload: JwtPayload = { id: this.id };
+    const token = createJWT(payload, expiresIn);
+    return {
+      user: this,
+      token: `Bearer ${token}`,
+    };
+  }
 }
