98f1d07cb4457c938e74dee3b2275c4369d8f2b9
phanduc0908 <phanduc0908@gmail.com>
2020-06-30 15:36:13 +0700

    Revert "fix: docker-compose"
    This reverts commit 6a4f56b4794aafa1d4c41dfabd414f503cc4883b.

---
+++ src/db/migrations/1593006975305-okrs.ts
@@ -0,0 +1,7 @@
+// import { MigrationInterface, QueryRunner } from 'typeorm';
+
+// export class okrs1593006975305 implements MigrationInterface {
+//   public async up(queryRunner: QueryRunner): Promise<void> {}
+
+//   public async down(queryRunner: QueryRunner): Promise<void> {}
+// }
---
+++ src/db/migrations/1593445272105-create_table_teams.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { TableName } from '@constants/Enums';
+
+export class createTableTeams1593445272105 implements MigrationInterface {
+  private teamsTable: Table = new Table({
+    name: TableName.Teams,
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isPrimary: true,
+        isGenerated: true,
+      },
+      {
+        name: 'name',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'createAt',
+        type: 'date',
+      },
+      {
+        name: 'updateAt',
+        type: 'date',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(this.teamsTable);
+  }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable(this.teamsTable);
+  }
+}
