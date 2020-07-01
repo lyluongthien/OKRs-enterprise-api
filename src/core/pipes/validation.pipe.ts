a5b1a14319b11f9563ef0060051822d131edd58b
Harry Tran <nhattq.coding@gmail.com>
2020-07-01 17:45:27 +0700

    fix: update code to pass codacy code quality

--- src/core/pipes/validation.pipe.ts
+++ src/core/pipes/validation.pipe.ts
@@ -4,11 +4,11 @@ import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
-  private readonly validationError = 0;
-  private readonly emptyValue = 0;
+  private readonly validationError: number = 0;
+  private readonly emptyValue: number = 0;
 
   /* eslint  @typescript-eslint/explicit-module-boundary-types: "off" */
-  async transform(value: any, { metatype }: ArgumentMetadata) {
+  public async transform(value: any, { metatype }: ArgumentMetadata) {
     if (value instanceof Object && this.isEmpty(value)) {
       throw new HttpException('Validation failed: No body submitted', HttpStatus.BAD_REQUEST);
     }
@@ -23,7 +23,7 @@ export class ValidationPipe implements PipeTransform {
     return value;
   }
 
-  private toValidate(metatype): boolean {
+  private toValidate(metatype: any): boolean {
     const types = [String, Boolean, Number, Array, Object];
     return !types.find((type) => metatype === type);
   }
