c6a62948557bd8ee33a2da485f28d92a86a0145b
quangnv281098 <quangkm44@gmail.com>
2020-07-11 16:35:10 +0700

    feat: update crud measure unit

--- src/modules/measure-unit/measure-unit.controller.ts
+++ src/modules/measure-unit/measure-unit.controller.ts
@@ -7,26 +7,31 @@ import { MeasureUnitEntity } from '@app/db/entities/measure-unit.entity';
 
 @Controller('/api/v1/measure-unit')
 export class MeasureUnitController {
-  constructor(private measureService: MeasureUnitService) {}
+  constructor(private _measureService: MeasureUnitService) {}
+
+  @Get()
+  public getAllMeasureUnit(): Promise<MeasureUnitEntity[]> {
+    return this._measureService.getListMeasureUnit();
+  }
+
+  @Get(':id')
+  public getMeasureUnitDetail(@Param('id') id: number): Promise<MeasureUnitEntity> {
+    return this._measureService.getMeasureUnitDetail(id);
+  }
 
   @Post()
   @UsePipes(new ValidationPipe())
   public createMeasureUnit(@Body() role: MeasureUnitDTO): Promise<MeasureUnitEntity> {
-    return this.measureService.createMeasureUnit(role);
-  }
-
-  @Get(':id')
-  private getMeasureUnitDetail(@Param('id') id: number): Promise<MeasureUnitEntity> {
-    return this.measureService.getMeasureUnitDetail(id);
+    return this._measureService.createMeasureUnit(role);
   }
 
   @Put(':id')
-  private updateMeasureUnit(@Param('id') id: number, @Body() data: MeasureUnitDTO): Promise<MeasureUnitEntity> {
-    return this.measureService.updateMeasureUnit(id, data);
+  public updateMeasureUnit(@Param('id') id: number, @Body() data: MeasureUnitDTO): Promise<MeasureUnitEntity> {
+    return this._measureService.updateMeasureUnit(id, data);
   }
 
   @Delete(':id')
-  private deleteMeasureUnit(@Param('id') id: number): any {
-    return this.measureService.deleteMeasureUnit(id);
+  public deleteMeasureUnit(@Param('id') id: number): any {
+    return this._measureService.deleteMeasureUnit(id);
   }
 }
--- src/modules/measure-unit/measure-unit.service.ts
+++ src/modules/measure-unit/measure-unit.service.ts
@@ -7,25 +7,25 @@ import { MeasureUnitDTO } from './measure-unit.dto';
 
 @Injectable()
 export class MeasureUnitService {
-  constructor(private measureRepository: MeasureRepository) {}
+  constructor(private _measureRepository: MeasureRepository) {}
 
   public getListMeasureUnit(): Promise<MeasureUnitEntity[]> {
-    return this.measureRepository.getList();
+    return this._measureRepository.getList();
   }
 
   public createMeasureUnit(data: MeasureUnitDTO): Promise<MeasureUnitEntity> {
-    return this.measureRepository.createMeasureUnit(data);
+    return this._measureRepository.createMeasureUnit(data);
   }
 
   public getMeasureUnitDetail(id: number): Promise<MeasureUnitEntity> {
-    return this.measureRepository.getMeasureUnitDetail(id);
+    return this._measureRepository.getMeasureUnitDetail(id);
   }
 
   public updateMeasureUnit(id: number, data: Partial<MeasureUnitDTO>): Promise<MeasureUnitEntity> {
-    return this.measureRepository.updateMeasureUnit(id, data);
+    return this._measureRepository.updateMeasureUnit(id, data);
   }
 
   public deleteMeasureUnit(id: number): Promise<ObjectLiteral> {
-    return this.measureRepository.deleteMeasureUnit(id);
+    return this._measureRepository.deleteMeasureUnit(id);
   }
 }
