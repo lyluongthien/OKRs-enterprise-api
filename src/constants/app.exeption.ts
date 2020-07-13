75d9a373e847e2254e864e50bd71b81d5102c2a6
Harry Tran <nhattq.coding@gmail.com>
2020-07-13 21:06:14 +0700

    chore: update const message of exceptions

--- src/constants/app.exeption.ts
+++ src/constants/app.exeption.ts
@@ -1,25 +1,7 @@
-import { HttpStatus } from '@nestjs/common';
-
-export const httpEmailExists = {
-  message: 'Email already exists',
-  errorCode: HttpStatus.CONFLICT,
-};
-
-export const httpUnauthenticated = {
-  message: `Not authenticated`,
-  statusCode: HttpStatus.FORBIDDEN,
-};
-
-export const httpUnauthorized = {
-  message: `Unauthorized access`,
-  statusCode: HttpStatus.UNAUTHORIZED,
-};
-
-export const httpDatabaseException = {
-  statusCode: HttpStatus.BAD_REQUEST,
-  message: `Exception when handle with database`,
-};
-
+export const httpEmailExists = 'Email already exists';
+export const httpUnauthenticated = `Not authenticated`;
+export const httpUnauthorized = `Unauthorized access`;
+export const httpDatabaseException = `Exception when handle with database`;
 export const invalidCredential = 'Invalid credentials';
 export const missingAuthenticationGuard = `Could not find user under execution context. 
 Please make sure AuthenticationGuard are defined first.`;
