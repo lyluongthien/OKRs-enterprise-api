5603f2977e8e4753886f61abbc5f82b29188ccb4
DucNM <minhduc.ngo0911@gmail.com>
2020-07-13 21:59:20 +0700

    fix: #31 resolve conflict role

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,29 +1,6 @@
-import { Controller, Post, UsePipes, Body, Param, Get, Put, Delete, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { RoleService } from './role.service';
-import { RoleEntity } from '@app/db/entities/role.entity';
-import { RoleDTO } from './role.dto';
 @Controller('roles')
 export class RoleController {
   constructor(private roleService: RoleService) {}
-
-  @Post()
-  @UsePipes(new ValidationPipe())
-  public createRole(@Body() role: RoleDTO): Promise<RoleEntity> {
-    return this.roleService.createRole(role);
-  }
-
-  @Get(':id')
-  private getDetailRole(@Param('id') id: number): Promise<RoleEntity> {
-    return this.roleService.getRoleDetail(id);
-  }
-
-  @Put(':id')
-  private updateRole(@Param('id') id: number, @Body() data: RoleDTO): Promise<RoleEntity> {
-    return this.roleService.updateRole(id, data);
-  }
-
-  @Delete(':id')
-  private deleteRole(@Param('id') id: number): any {
-    return this.roleService.deleteRole(id);
-  }
 }
--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,31 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ObjectLiteral } from 'typeorm';
-
 import { RoleRepository } from './role.repository';
-import { RoleDTO } from './role.dto';
-import { RoleEntity } from '@app/db/entities/role.entity';
 
 @Injectable()
 export class RoleService {
   constructor(private roleRepository: RoleRepository) {}
-
-  public getListRole(): Promise<RoleEntity[]> {
-    return this.roleRepository.getList();
-  }
-
-  public createRole(data: RoleDTO): Promise<RoleEntity> {
-    return this.roleRepository.createRole(data);
-  }
-
-  public getRoleDetail(id: number): Promise<RoleEntity> {
-    return this.roleRepository.getRoleDetail(id);
-  }
-
-  public updateRole(id: number, data: RoleDTO): Promise<RoleEntity> {
-    return this.roleRepository.updateRole(id, data);
-  }
-
-  public deleteRole(id: number): Promise<ObjectLiteral> {
-    return this.roleRepository.deleteRole(id);
-  }
 }
