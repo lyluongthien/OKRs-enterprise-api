9d0474c4c256daf313e7d9c179164774f2e4ba75
quangnv281098 <quangkm44@gmail.com>
2020-07-22 20:11:17 +0700

    fix: fix codacy ranking star (#112)

--- src/modules/user-star/user-star.repository.ts
+++ src/modules/user-star/user-star.repository.ts
@@ -7,8 +7,8 @@ import { CommonMessage } from '@app/constants/app.enums';
 export class UserStarRepository extends Repository<UserStarEntity> {
   public async getAllUserStar(): Promise<UserStarEntity[]> {
     try {
-      return await this
-        .query(`SELECT "user"."fullName" AS "user_fullName", "user"."id" AS "user_id", SUM("user_stars"."star") AS "sum" 
+      return await this.query(`SELECT "user"."fullName" AS "user_fullName", "user"."id" AS "user_id", 
+              SUM("user_stars"."star") AS "sum" 
               FROM "user_stars" "user_stars" LEFT JOIN "users" "user" ON "user"."id"="user_stars"."userId" 
               WHERE "cycleId" in (select "cycleId" from cycles c2) GROUP BY "user"."id", "user"."fullName"
               ORDER BY "sum" desc`);
@@ -19,8 +19,8 @@ export class UserStarRepository extends Repository<UserStarEntity> {
 
   public async getCurrentUserStar(id: number): Promise<UserStarEntity[]> {
     try {
-      return await this
-        .query(`SELECT "user"."fullName" AS "user_fullName", "user"."id" AS "user_id", SUM("user_stars"."star") AS "sum" 
+      return await this.query(`SELECT "user"."fullName" AS "user_fullName", "user"."id" AS "user_id", 
+              SUM("user_stars"."star") AS "sum" 
               FROM "user_stars" "user_stars" LEFT JOIN "users" "user" ON "user"."id"="user_stars"."userId" 
               WHERE "cycleId" in (${id}) GROUP BY "user"."id", "user"."fullName"
               ORDER BY "sum" desc`);
