e365b5ef5d6acb91c18a5fc10d5bffeecc7558cc
quangnv281098 <quangkm44@gmail.com>
2020-08-15 23:39:31 +0700

    feat: add view okr status dashboard

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -7,6 +7,7 @@ import { UserRepository } from '../user/user.repository';
 import { RoleRepository } from '../role/role.repository';
 import { ObjectiveRepository } from '../objective/objective.repository';
 import { RecognitionRepository } from '../recognition/recognition.repository';
+import { CheckinRepository } from '../checkin/checkin.repository';
 
 @Injectable()
 export class DashboardService {
@@ -16,6 +17,7 @@ export class DashboardService {
     private _userRepository: UserRepository,
     private _objectiveRepository: ObjectiveRepository,
     private _roleRepository: RoleRepository,
+    private _checkinRepository: CheckinRepository,
   ) {}
 
   public async getTopStars(cycleId: number, type: TopStarType): Promise<ResponseModel> {
@@ -40,7 +42,7 @@ export class DashboardService {
     };
   }
 
-  public async getFirstAndLastDate(): Promise<ResponseModel> {
+  public async getCFRsStatus(): Promise<ResponseModel> {
     const today = new Date();
     const first = today.getDate() - today.getDay() + 1;
     const last = first + 6;
@@ -85,11 +87,100 @@ export class DashboardService {
   }
 
   public async getOKRsStatus(): Promise<ResponseModel> {
-    const data = null;
+    const today = new Date();
+    const first = today.getDate() - today.getDay() + 1;
+    const last = first + 6;
+
+    const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+    const firstOfLastWeek = lastWeek.getDate() - lastWeek.getDay() + 1;
+    const lastOfLastWeek = firstOfLastWeek + 6;
+
+    const firstday = new Date(today.setDate(first)).toISOString();
+    const lastday = new Date(today.setDate(last)).toISOString();
+
+    const firstDayOfLastWeek = new Date(lastWeek.setDate(firstOfLastWeek)).toISOString();
+    const lastDayOfLastWeek = new Date(lastWeek.setDate(lastOfLastWeek)).toISOString();
+
+    const dataCurrentWeek = await this._checkinRepository.getOKRStatus(firstday, lastday);
+    const dataLastWeek = await this._checkinRepository.getOKRStatus(firstDayOfLastWeek, lastDayOfLastWeek);
+
+    const notYetCheckinCurrentWeek = await this._checkinRepository.getNotYetCheckinStatus(firstday, lastday);
+    const notYetCheckinLastWeek = await this._checkinRepository.getNotYetCheckinStatus(
+      firstDayOfLastWeek,
+      lastDayOfLastWeek,
+    );
+
+    let good = 0,
+      normal = 0,
+      bad = 0,
+      very_bad = 0;
+
+    notYetCheckinCurrentWeek.map((value) => {
+      very_bad += value.notyetcheckin;
+      return value;
+    });
+    dataCurrentWeek.map((value) => {
+      if (value.progresschanging === 0) very_bad++;
+      else {
+        if (value.confidentLevel === 1) bad++;
+        else if (value.confidentLevel === 2) normal++;
+        else if (value.confidentLevel === 3) good++;
+      }
+      return value;
+    });
+
+    const dataResponseCurrentWeek = {
+      good: good,
+      normal: normal,
+      bad: bad,
+      very_bad: very_bad,
+    };
+
+    good = normal = bad = very_bad = 0;
+
+    notYetCheckinLastWeek.map((value) => {
+      very_bad += value.notyetcheckin;
+      return value;
+    });
+    dataLastWeek.map((value) => {
+      if (value.progresschanging === 0) very_bad++;
+      else {
+        if (value.confidentLevel === 1) bad++;
+        else if (value.confidentLevel === 2) normal++;
+        else if (value.confidentLevel === 3) good++;
+      }
+    });
+
+    const dataResponseLastWeek = {
+      good: good,
+      normal: normal,
+      bad: bad,
+      very_bad: very_bad,
+    };
+
+    const dataResponse: any = [
+      {
+        good: dataResponseCurrentWeek.good,
+        changing: dataResponseCurrentWeek.good - dataResponseLastWeek.good,
+      },
+      {
+        normal: dataResponseCurrentWeek.normal,
+        changing: dataResponseCurrentWeek.normal - dataResponseLastWeek.normal,
+      },
+      {
+        bad: dataResponseCurrentWeek.bad,
+        changing: dataResponseCurrentWeek.bad - dataResponseLastWeek.bad,
+      },
+      {
+        very_bad: dataResponseCurrentWeek.very_bad,
+        changing: dataResponseCurrentWeek.very_bad - dataResponseLastWeek.very_bad,
+      },
+    ];
+
     return {
       statusCode: HttpStatus.OK,
       message: CommonMessage.SUCCESS,
-      data: data,
+      data: dataResponse,
     };
   }
 }
