12c861e12e3786b1433e1ccceb08248ae0d09c4a
quangnv281098 <quangkm44@gmail.com>
2020-09-07 11:41:27 +0700

    fix: fix view list OKRs

--- src/modules/objective/__test__/objective.unit.spec.ts
+++ src/modules/objective/__test__/objective.unit.spec.ts
@@ -29,7 +29,7 @@ describe('Objective Service', () => {
     it('test create OKRs', async () => {
       const someMockedUsers: any = {};
       someMockedUsers.objective = {
-        title: 'TestOKRs222',
+        title: 'Test Create OKRs',
         parentObjectiveId: 20,
         cycleId: 3,
         isRootObjective: false,
@@ -40,6 +40,37 @@ describe('Objective Service', () => {
       const res = await objectiveService.createAndUpdateOKRs(someMockedUsers, connection.manager);
       expect(res).toBeDefined();
       expect(res.statusCode).toEqual(200);
+    });
+
+    it('test update OKRs', async () => {
+      const someMockedUsers: any = {};
+      someMockedUsers.objective = {
+        id: 20,
+        userId: 13,
+        title: 'Test Update OKRs',
+        parentObjectiveId: 4,
+      };
+      someMockedUsers.keyResult = [
+        { startValue: 0, targetValue: 100, content: 'KR1', linkPlans: '', linkResults: '', measureUnitId: 1 },
+      ];
+      const res = await objectiveService.createAndUpdateOKRs(someMockedUsers, connection.manager);
+      expect(res).toBeDefined();
+      expect(res.statusCode).toEqual(200);
+    });
+    it('test get OKRs detail', async () => {
+      const expectedResult = {
+        objective: {
+          title: 'TESt OKR member',
+          progress: 41,
+        },
+        keyResult: [{}],
+      };
+      const res = await objectiveService.getDetailOKRs(20);
+      expect(res).toBeDefined();
+      expect(res.statusCode).toEqual(200);
+      expect(res.data.objective.title).toEqual(expectedResult.objective.title);
+      expect(res.data.objective.progress).toEqual(expectedResult.objective.progress);
+      expect(res.data.keyResult.length).toEqual(expectedResult.keyResult.length);
     });
   });
 });
--- src/modules/objective/objective.repository.ts
+++ src/modules/objective/objective.repository.ts
@@ -170,7 +170,7 @@ export class ObjectiveRepository extends Repository<ObjectiveEntity> {
           case OKRsType.ROOT:
             return await queryBuilder
               .andWhere('objective.isRootObjective = :root', { root: true })
-              .andWhere('childUser.isActive = true')
+              .andWhere('coalesce(childUser.isActive, true) = true')
               .getMany();
           case OKRsType.PERSONAL:
             return await queryBuilder
@@ -180,7 +180,7 @@ export class ObjectiveRepository extends Repository<ObjectiveEntity> {
           case OKRsType.TEAM:
             return await queryBuilder
               .andWhere('users.id = :user', { user: id })
-              .andWhere('childUser.isActive = true')
+              .andWhere('coalesce(childUser.isActive, true) = true')
               .getMany();
           default:
             return await queryBuilder.getMany();
